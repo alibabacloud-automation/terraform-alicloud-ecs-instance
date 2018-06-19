93a776911e9c759288fdaa0a1142c71afc00c408
angryninja48 <jonbaker85@gmail.com>
2018-06-19 17:49:14 +1000

    Fix typo

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "alicloud_disk_attachment" "disk_attach" {
 }
 
 // Attach key pair to instances for Module
-resource "alicloud_key_pair_attchment" "default" {
+resource "alicloud_key_pair_attachment" "default" {
   count = "${var.number_of_instances > 0 && var.key_name != "" ? 1 : 0}"
 
   key_name = "${var.key_name}"
